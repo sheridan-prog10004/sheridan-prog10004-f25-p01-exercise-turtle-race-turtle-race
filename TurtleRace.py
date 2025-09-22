a676dfd0d933e35b5587b5e69744a6a7ffb7bd72
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-09-22 14:34:45 -0400

    Position racers on the starting line.

--- TurtleRace.py
+++ TurtleRace.py
@@ -9,9 +9,34 @@ import turtle
 raceTrack = turtle.Screen()
 raceTrack.bgcolor("lightgreen")
 
-#create the racers
+#create the racers: larry, curly and moe
+larry = turtle.Turtle()
+larry.shape("turtle")
+larry.shapesize(3,3,1)
+larry.color("darkgreen")
+larry.penup()
+
+curly = turtle.Turtle()
+curly.shape("turtle")
+curly.shapesize(3, 3, 1)
+curly.color("darkred")
+curly.penup()
+
+moe = turtle.Turtle()
+moe.shape("turtle")
+moe.shapesize(3, 3, 1)
+moe.color("blue")
+moe.penup()
 
 #position the racers on the starting line
+larry.left(180)
+larry.forward(200)
+larry.right(90)
+
+curly.left(90)
+
+moe.forward(200)
+moe.left(90)
 
 #run the race
 
