925f99b437dce43f9c818945e84436880fdbc583
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-09-24 16:23:47 -0400

    Divided the structure of the program into functions the communicate with each others via parameters.

--- TurtleRace.py
+++ TurtleRace.py
@@ -7,56 +7,72 @@ import turtle
 import random
 
 #create the race track/window where the race takes place
-raceTrack = turtle.Screen()
-raceTrack.bgcolor("lightgreen")
+def createRaceTrack():
+    raceTrack = turtle.Screen()
+    raceTrack.bgcolor("lightgreen")
+    return raceTrack
 
 #create the racer variables/objects
-leo = turtle.Turtle()
-leo.shape("turtle")
-leo.shapesize(3,3,1)
-leo.color("darkgreen")
-leo.penup()
+def registerRacers():
+    leo = turtle.Turtle()
+    leo.shape("turtle")
+    leo.shapesize(3,3,1)
+    leo.color("darkgreen")
+    leo.penup()
 
-mikey = turtle.Turtle()
-mikey.shape("turtle")
-mikey.shapesize(3, 3, 1)
-mikey.color("darkred")
-mikey.penup()
+    mikey = turtle.Turtle()
+    mikey.shape("turtle")
+    mikey.shapesize(3, 3, 1)
+    mikey.color("darkred")
+    mikey.penup()
 
-don = turtle.Turtle()
-don.shape("turtle")
-don.shapesize(3, 3, 1)
-don.color("blue")
-don.penup()
+    don = turtle.Turtle()
+    don.shape("turtle")
+    don.shapesize(3, 3, 1)
+    don.color("blue")
+    don.penup()
+    return leo,mikey,don
 
 #position the racers on the starting line
-leo.left(180)
-leo.forward(200)
-leo.right(90)
+def positionRacers(leo, mikey, don):
+    leo.left(180)
+    leo.forward(200)
+    leo.right(90)
 
-mikey.left(90)
+    mikey.left(90)
 
-don.forward(200)
-don.left(90)
+    don.forward(200)
+    don.left(90)
 
-#prepare the racers for the race
-leo.pendown()
-mikey.pendown()
-don.pendown()
+    #prepare the racers for the race
+    leo.pendown()
+    mikey.pendown()
+    don.pendown()
 
 #run the race
-dist = random.randint(10, 100)
-leo.forward(dist)
+def runRace(leo, mikey, don):
+    dist = random.randint(10, 100)
+    leo.forward(dist)
 
-dist = random.randint(10, 100)
-mikey.forward(dist)
+    dist = random.randint(10, 100)
+    mikey.forward(dist)
 
-dist = random.randint(10, 100)
-don.forward(dist)
+    dist = random.randint(10, 100)
+    don.forward(dist)
 
 #determine the winner
+def determineWinner(leo, mikey, done):
+    #TODO: Implement determining winner
 
-#announce the winner
+    #announce the winner
+    pass
+
+#run the main program statements
+raceTrack = createRaceTrack()
+racer1, racer2, racer3 = registerRacers()
+positionRacers(racer1, racer2, racer3)
+runRace(racer1, racer2, racer3)
+determineWinner(racer1, racer2, racer3)
 
 #wait for the user to click before exiting
 raceTrack.exitonclick()
