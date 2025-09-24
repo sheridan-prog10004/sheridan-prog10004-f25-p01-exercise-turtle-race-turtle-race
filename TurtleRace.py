7703b9fcd52d7352a404948b07f5df99ebf01e1d
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-09-24 14:56:55 -0400

    Use refactoring to rename variables.

--- TurtleRace.py
+++ TurtleRace.py
@@ -4,41 +4,55 @@ Version 1: In this version, the turtle race has a falt single-file structure
 
 #access the libraries/modules required for the program
 import turtle
+import random
 
 #create the race track/window where the race takes place
 raceTrack = turtle.Screen()
 raceTrack.bgcolor("lightgreen")
 
-#create the racers: larry, curly and moe
-larry = turtle.Turtle()
-larry.shape("turtle")
-larry.shapesize(3,3,1)
-larry.color("darkgreen")
-larry.penup()
+#create the racer variables/objects
+leo = turtle.Turtle()
+leo.shape("turtle")
+leo.shapesize(3,3,1)
+leo.color("darkgreen")
+leo.penup()
 
-curly = turtle.Turtle()
-curly.shape("turtle")
-curly.shapesize(3, 3, 1)
-curly.color("darkred")
-curly.penup()
+mikey = turtle.Turtle()
+mikey.shape("turtle")
+mikey.shapesize(3, 3, 1)
+mikey.color("darkred")
+mikey.penup()
 
-moe = turtle.Turtle()
-moe.shape("turtle")
-moe.shapesize(3, 3, 1)
-moe.color("blue")
-moe.penup()
+don = turtle.Turtle()
+don.shape("turtle")
+don.shapesize(3, 3, 1)
+don.color("blue")
+don.penup()
 
 #position the racers on the starting line
-larry.left(180)
-larry.forward(200)
-larry.right(90)
+leo.left(180)
+leo.forward(200)
+leo.right(90)
 
-curly.left(90)
+mikey.left(90)
 
-moe.forward(200)
-moe.left(90)
+don.forward(200)
+don.left(90)
+
+#prepare the racers for the race
+leo.pendown()
+mikey.pendown()
+don.pendown()
 
 #run the race
+dist = random.randint(10, 100)
+leo.forward(dist)
+
+dist = random.randint(10, 100)
+mikey.forward(dist)
+
+dist = random.randint(10, 100)
+don.forward(dist)
 
 #determine the winner
 
